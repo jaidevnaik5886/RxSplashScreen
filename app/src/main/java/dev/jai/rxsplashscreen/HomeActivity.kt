9e32da0446443b18503b1ce9eb9a6282b4176809
Jaidev Naik <jaidev.naik@transerve.com>
2019-06-19 16:11:36 +0530

    -functions changed

--- app/src/main/java/dev/jai/rxsplashscreen/HomeActivity.kt
+++ app/src/main/java/dev/jai/rxsplashscreen/HomeActivity.kt
@@ -9,7 +9,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
     }
 }
 
